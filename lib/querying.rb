778e99a4fe31925ac805e7d3a281de58bf75b093
German Cruz <cruzgerman216@yahoo.com>
2020-04-23 20:42:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,9 +13,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name from authors
-  JOIN series ON authors.id = series.author_id
-  JOIN subgenres ON subgenres.id = series.subgenre_id"
+  "SELECT characters.name,  COUNT(character_books.character_id) FROM characters
+join character_books ON character_books.character_id = characters.id
+GROUP BY characters.id ORDER BY COUNT(character_books.character_id) DESC"
 end
 
 def select_series_title_with_most_human_characters
